897b16c30aed9750c9d4bd0adb8bc8906834fca6
Anatoly <gusev.toia@yandex.ru>
2020-04-16 13:13:11 +0300

    Refactoring project structure

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = '{{ cookiecutter.project_slug }}.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = '{{ cookiecutter.project_slug }}.wsgi.application'
 
 # --------------------------------------------------------------------------------------------------
 # - DATABASES --------------------------------------------------------------------------------------
