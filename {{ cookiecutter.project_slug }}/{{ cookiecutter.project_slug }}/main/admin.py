ebf5fdbba67de4d2f13c8a30f57b9ecfd7840ef2
Anatoly Gusev <gusev.tolia@yandex.ru>
2020-08-18 12:35:00 +0300

    Remove `remove_built_in_user_and_group_from_admin` settings

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/main/admin.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/main/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-{% if cookiecutter.remove_built_in_user_and_group_from_admin == 'y' %}
-from django.contrib.auth.models import User, Group
-
-admin.site.unregister(User)
-admin.site.unregister(Group)
-{% endif %}
