ed07db5814b2ceb7c2fdcf80f3516baabe3146ff
Anatoly Gusev <gusev.tolia@yandex.ru>
2020-08-18 12:31:29 +0300

    Remove `custom_404_page` setting

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/main/views.py
+++
@@ -1,7 +0,0 @@
-{%- if cookiecutter.custom_404_page == 'y' -%}
-from django.shortcuts import render
-
-
-def handler404(request, exception=None, template_name='404.html'):
-    return render(request, template_name, status='404')
-{%- endif -%}
--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/urls.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/urls.py
@@ -2,9 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-{% if cookiecutter.custom_404_page == 'y' -%}
-from {{ cookiecutter.project_slug }}.main.views import handler404
-{%- endif %}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,10 +9,3 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    {% if cookiecutter.custom_404_page == 'y' -%}
-    urlpatterns.append(path('404/', handler404))
-    {%- endif %}
-
-{% if cookiecutter.custom_404_page == 'y' -%}
-handler404 = '{{ cookiecutter.project_slug }}.main.views.handler404'
-{%- endif %}
