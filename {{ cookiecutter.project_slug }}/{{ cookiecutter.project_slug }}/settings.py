15ece67989dcaf5eb8366a5f1d82140a24bbdb50
Anatoly Gusev <gusev.tolia@yandex.ru>
2021-09-01 10:42:12 +0300

    added DEFAULT_AUTO_FIELD

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/settings.py
@@ -16,6 +16,8 @@ SECRET_KEY = env("DJANGO_SECRET_KEY", default="!!! SET SECRET KEY !!!")
 
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 
+DEFAULT_AUTO_FIELD = 'django.db.models.fields.AutoField'
+
 
 # DATABASES
 # ------------------------------------------------------------------------------
