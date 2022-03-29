fa9929720e517661ff20081e74a630f7647f36ee
Anatoly <gusev.tolia@yandex.ru>
2022-03-29 17:48:10 +0300

    Fix swagger schema by drf-yasg

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/schema/urls.py
+++
@@ -1,9 +0,0 @@
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from django.urls import path
-
-app_name = "docs"
-
-urlpatterns = [
-    path("", SpectacularSwaggerView.as_view(url_name="docs:schema"), name="swagger-ui"),
-    path("swagger.json", SpectacularAPIView.as_view(), name="schema"),
-]
--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/urls.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/docs/", include("{{ cookiecutter.project_slug }}.schema.urls")),
+    path("api/docs/", include("{{ cookiecutter.project_slug }}.swagger.urls")),
 ]
 
 if settings.DEBUG:
